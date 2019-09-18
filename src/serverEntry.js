b757a083c69b152b49304a5569bf4284f9b3383b
Brandon Victor <Multihuntr@hotmail.com>
2019-09-18 12:46:08 +1000

    Added a process trigger to ensure sigint killed the process

--- src/serverEntry.js
+++ src/serverEntry.js
@@ -10,6 +10,10 @@ require('@babel/register')();
 
 const serverMain = require('./server').default;
 
+// Ensure we can ctrl+c the process
+process.on('SIGINT', function() {
+  process.exit(1);
+});
 
 // Call the server-side main function.
 serverMain(process.argv);
